ae085efc742895d9a0cb500f20af8408ef082508
Shreyans Bhansali <shreyans@venmo.com>
2013-01-22 12:18:37 -0500

    removed static_old

--- static_old/js/user.js
+++
@@ -1,6 +0,0 @@
-sole.user = sole.user || {};
-sole.user.user = {
-    'id': 1,
-    'name': 'Shreyans Bhansali',
-    "img": "https://venmopics.appspot.com/u/v22/s/01fcf991-3592-4e40-bc8b-df3eb0c1f027"
-};
