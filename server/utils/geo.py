41255bf4d43ed1be83c566219ae07ab0e01b7c71
Shreyans Bhansali <shreyans@venmo.com>
2013-01-25 15:37:12 -0500

    TODO updates and geoip path update using APP_HOME env

--- server/utils/geo.py
+++ server/utils/geo.py
@@ -1,8 +1,11 @@
 import pygeoip
+import os
 
-geo = pygeoip.GeoIP('data/GeoLiteCity.dat')
+geoip_path = os.path.join(os.environ.get('APP_HOME'), 'data/GeoLiteCity.dat')
+geo = pygeoip.GeoIP(geoip_path)
 
 def loc_from_ip(ip):
     if ip == "127.0.0.1":
-        ip = "108.27.114.30"
+        ip = "108.27.114.30" # New York, NY
+        ip = "208.76.220.220" # Philadelphia, PA
     return geo.record_by_addr(ip)
