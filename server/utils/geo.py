40866c97847cc76f6c38929e641cf6e61c1282a3
Shreyans Bhansali <shreyans@venmo.com>
2013-01-27 15:34:15 -0500

    TODOs moved to Asana

--- server/utils/geo.py
+++ server/utils/geo.py
@@ -6,7 +6,7 @@ geo = pygeoip.GeoIP(geoip_path)
 
 def loc_from_ip(ip):
     if ip == "127.0.0.1":
-        ip = "108.27.114.30" # New York, NY
         ip = "223.191.0.222" # Mumbai, India
         ip = "130.91.1.1" # Philadelphia, PA
+        ip = "108.27.114.30" # New York, NY
     return geo.record_by_addr(ip)
